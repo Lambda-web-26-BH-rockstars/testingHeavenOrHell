134e40c5c88233328cc46d1a973c5ee3c7cae2e0
Brian Hague <alphaseinor@gmail.com>
2020-01-29 15:26:01 -0600

    added react app that does something next to nothing

--- hoh/src/App.js
+++ hoh/src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [pictures, setPictures] = useState([
+    {
+      name: "coffee",
+      path: "./images/coffee.jpg",
+      description: "image of a woman drinking coffee"
+    },
+    {
+      name: "crazy cat",
+      path: "./images/crazy_cat.jpg",
+      description: "image of an insane cat"
+    },
+    {
+      name: "russian granola",
+      path: "./images/russian_granola.jpg",
+      description: "image of granola from russia"
+    },
+    {
+      name: "street life",
+      path: "./images/street.jpg",
+      description: "image of a man riding his bike in tokyo"
+    }
+  ])
+
+  const [imageNumber, setImageNumber] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{pictures[imageNumber].name}</p>
+      <img src={pictures[imageNumber].path} aria-label={pictures[imageNumber].description} />
+      <button
+        onClick={()=> {
+
+          setImageNumber(imageNumber === 0 ? 3 : imageNumber-1)
+        }}
+      >
+        Previous
+      </button>
+      <button
+        onClick={()=> {
+          setImageNumber((imageNumber+1) % 4)
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 }
